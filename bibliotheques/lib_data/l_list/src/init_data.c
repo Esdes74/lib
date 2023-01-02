502b97bce72d2d9fd777300fc26d0a9db052d7ab
Esdes74 <115540633+Esdes74@users.noreply.github.com>
2023-01-02 19:59:24 +0100

    Update init_data.c

---
+++ bibliotheques/lib_data/l_list/src/init_data.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_data.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eslamber <eslamber@student.42.ft>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/02 18:38:58 by eslamber          #+#    #+#             */
+/*   Updated: 2023/01/02 19:59:11 by eslamber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../lib_list.h"
+
+t_data	*init_data(void *d, t_type t, t_bool allow, t_data *cell)
+{
+	cell->allowed = allow;
+	cell->type_data = t;
+	cell->data = d;
+	return (cell);
+}
+
