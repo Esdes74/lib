e47a4d36c977940528cf7ae89413ac4ffb04ba4a
Esdes74 <115540633+Esdes74@users.noreply.github.com>
2023-02-11 14:18:36 +0100

    test pour clear_cell.c, a tester et a réfléchire plus

--- bibliotheques/lib_data/l_gnrl/src/clear_cell.c
+++ bibliotheques/lib_data/l_gnrl/src/clear_cell.c
@@ -6,7 +6,7 @@
 /*   By: eslamber <eslamber@student.42.ft>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 21:15:52 by eslamber          #+#    #+#             */
-/*   Updated: 2023/02/10 21:24:31 by eslamber         ###   ########.fr       */
+/*   Updated: 2023/02/11 14:17:56 by eslamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 void	clear_cell(t_cell *cell, void (*f)(void *))
 {
-	
+	if (cell->next != 0)
+		cell->next->prev = cell->prev;
+	if (cell->prev != 0)
+		cell->prev->next = cell->next;
+	f(cell->data_cell->data);
+	free(cell->data_cell);
+	free(cell);
 }
